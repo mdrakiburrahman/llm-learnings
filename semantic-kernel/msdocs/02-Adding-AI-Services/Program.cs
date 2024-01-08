b45f076442350b1a580fa8b814e9a95a7bfda56f
Raki <46581776+mdrakiburrahman@users.noreply.github.com>
2024-01-07 20:25:02 -0500

    C# MSDocs tutorials

---
+++ semantic-kernel/msdocs/02-Adding-AI-Services/Program.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using Microsoft.SemanticKernel;
+
+var LlmService = Env.Var("Global:LlmService")!;
+
+if (LlmService == "AzureOpenAI")
+{
+    var AzureOpenAIDeploymentType = Env.Var("AzureOpenAI:DeploymentType")!;
+
+    if (AzureOpenAIDeploymentType == "chat-completion")
+    {
+        // Create a kernel with an Azure OpenAI chat completion service
+        //////////////////////////////////////////////////////////////////////////////////
+        var AzureOpenAIDeploymentName = Env.Var("AzureOpenAI:ChatCompletionDeploymentName")!;
+        var AzureOpenAIEndpoint = Env.Var("AzureOpenAI:Endpoint")!;
+        var AzureOpenAIApiKey = Env.Var("AzureOpenAI:ApiKey")!;
+        var pluginDirectory = Path.Combine(Directory.GetCurrentDirectory(), "plugins", "WriterPlugin");
+
+        var builder = Kernel.CreateBuilder();
+        builder.Services.AddAzureOpenAIChatCompletion(
+            deploymentName: AzureOpenAIDeploymentName,  // The name of your deployment (e.g., "gpt-35-turbo")
+            endpoint: AzureOpenAIEndpoint,              // The endpoint of your Azure OpenAI service
+            apiKey: AzureOpenAIApiKey                   // The API key of your Azure OpenAI service
+        );
+        builder.Plugins.AddFromPromptDirectory(pluginDirectory);
+        var kernel = builder.Build();
+
+        var result = await kernel.InvokeAsync("WriterPlugin", "ShortPoem", new() {
+            { "input", "Hello world" }
+        });
+        Console.WriteLine(result);
+    }
+    else
+    {
+        throw new Exception($"The Azure OpenAI deployment type '{AzureOpenAIDeploymentType}' is not supported.");
+    }
+}
+else
+{
+    throw new Exception($"The LLM service '{LlmService}' is not supported.");
+}
